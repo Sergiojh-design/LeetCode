b6f41243ea29356a8f53b1e109b062231331007a
Sergio Herrera <70117199+Sergiojh-design@users.noreply.github.com>
2024-02-08 20:13:52 -0800

    Time: 69 ms (37.82%), Space: 53.8 MB (30.12%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    let l = 0; r = 0;
+    let l = 0, r = 0;
     while(r < nums.length){
         if(nums[r] === 0) k--;
         if(k < 0){
             if(nums[l] === 0) k++;
-            l++
+            l++;
         }
         r++;
     }
